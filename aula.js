d689a993dd5701dcb678c57057466fc192df6998
ramon405 <douglasllc2@hotmail.com>
2023-11-20 21:43:36 -0300

    calculo imc

--- aula.js
+++ aula.js
@@ -1,19 +1,22 @@
-/*calcular  a media de notas dp semestre...  problemas da nossa vida real*/
-const nota1 = 8;
-const nota2 = 7;
-const nota3 = 7;
-const media = (nota1 + nota2 + nota3) / 3;
+/*calcular  o imc de um adulto...  problemas da nossa vida real*/
+const peso = 60;
+const altura  = 1.70;
+const imc = peso / Math.pow(altura,2);
 
-console.log(media);
+console.log(imc);
 
-if (media < 5) {
-  console.log('reprovado');
+if(imc < 18.5) {
+  console.log('Abaixo do Peso');
 }
-
-else if (media >= 5 && media <=7) {
-    console.log('recuperacao');
+else if (imc >= 18.5 && imc <= 24.9){
+  console.log('Peso Ideal');
 }
-
-else  {
-    console.log ('passou no semestre');
-};
+else if (imc > 24.9 && imc <= 29.9){
+  console.log('Acima do Peso');
+}
+else if (imc > 30.0 && imc <= 39.9){
+  console.log('obeso');
+}
+else{
+  console.log('Obesidade grave');
+}
